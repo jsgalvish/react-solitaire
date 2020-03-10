6781d167aa8ca9261186b1c41342450be81f09e7
JS Galvis H <jsgalvish@unal.edu.co>
2020-03-09 20:48:37 -0500

    Updates

---
+++ precache-manifest.a62d032df829df55135e2013b2886b94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1eaeae09da24db6125fef0b6ea094c0",
+    "url": "/react-solitaire/index.html"
+  },
+  {
+    "revision": "53f574b5c480e2f955d4",
+    "url": "/react-solitaire/static/css/main.4122c210.chunk.css"
+  },
+  {
+    "revision": "d5a9dcaa7648c5521028",
+    "url": "/react-solitaire/static/js/2.45045f1a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-solitaire/static/js/2.45045f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f574b5c480e2f955d4",
+    "url": "/react-solitaire/static/js/main.041b555f.chunk.js"
+  },
+  {
+    "revision": "dda2f86dec638c53f20d",
+    "url": "/react-solitaire/static/js/runtime-main.cf04ac0d.js"
+  }
+]);
